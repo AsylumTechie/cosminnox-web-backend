fd2a2f78c8e38b2fc71f301c7baa792afe482069
AsylumTechie <rohitchoudhari0011@gmail.com>
2025-04-04 14:08:05 +0530

    Updated origin

--- server.js
+++ server.js
@@ -6,41 +6,42 @@ import Contact from "./model/Contact.js";
 
 const app = express();
 
-console.log("Hello1")
-
-app.use(cors({
+app.use(
+  cors({
     origin: "https://cosminnox-web-frontend.vercel.app",
-    methods: ["GET", "POST"], 
-    allowedHeaders: ["Content-Type"], 
-  }));
-  
-app.use(json()); 
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.use(json());
 dotenv.config();
 
 connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
-app.post("/api/contact", async (req, res) => {
-
-console.log("Hello2")
+app.post("/", async (req, res) => {
   try {
     const { name, email, phone, message } = req.body;
 
     const newContact = new Contact({ name, email, phone, message });
     await newContact.save();
 
-    res.status(201).json({ success: true, message: "Message received successfully!" });
+    res
+      .status(201)
+      .json({ success: true, message: "Message received successfully!" });
   } catch (error) {
     console.error(error);
-res.status(500).json({ success: false, message: "Server Error", error: error.message });
-
+    res
+      .status(500)
+      .json({ success: false, message: "Server Error", error: error.message });
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("Contact Form Backend Running");
-});
+// app.get("/", (req, res) => {
+//   res.send("Contact Form Backend Running");
+// });
 
 const PORT = process.env.PORT || 5009;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
